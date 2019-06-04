ee808ab4cfc8a525bb15dd9010e68a956e7423d2
Jefferson <jefferson_jazzs@hotmail.com>
2019-06-03 22:42:46 -0300

    add commit - branch new

--- src/teste/Test.java
+++ src/teste/Test.java
@@ -11,7 +11,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class Test {
-
+//add commit
 	public static void main(String[] args) {
 		Properties props = new Properties();
 
